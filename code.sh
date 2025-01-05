e7c6d7cf100d7f7e367184aa2b7525a5dd768507
saikamal33 <kamaleshsai33@gmail.com>
2025-01-05 15:28:11 +0530

    final code for checking users access to a repo

--- code.sh
+++ code.sh
@@ -5,9 +5,22 @@
 #date: 3/1/2025
 # script to list out the user who has access to repository, with this we will recreate this command "curl -s -u "USERNAME:PASSWORD" https://api.github.com/user/repos/{owner}/{repo}/collaborators and filter the output#
 ################################
+# syntex on executing the script:
+##"./code.sh <owner(or)organisation_name> <repository_name>"##
+###############################
 
-set -x
-#to get the git url
+#checks if two aurguments are provided
+function helper {
+        command_aurg=2
+        if [ $# -ne $command_aurg ]; then
+                echo "Please provide owner/organisation and repository name"
+                exit 1 #exits the code if aurguments are not provided
+        fi
+}
+
+
+
+helper "$@" #passes aurg to helper function"
 
 API_URL="https://api.github.com"
 
@@ -22,11 +35,10 @@ Token=$token
 owner=$1
 repo=$2
 
-
-
 #we will make a function for git to make a github API request.
 
 function github_api {
+# we describe local user again as we only be used here in github_abi function	
 	local part="$1"
 	local url="${API_URL}/${part}"
 
@@ -42,17 +54,15 @@ function github_api {
 
 #we will filter users with read access "https://api.github.com/repos/{owner}/{repo}/collaborators"
 
-function github_list {
-	local end="repos/${owner}/${repo}/collaborators"
-	collab="$(github_api "$end")"
+github_list="repos/${owner}/${repo}/collaborators"
+collab="$(github_api "$github_list"| jq -r '.[] | select(.permissions.pull == true) | .login')" 
 
-#| jq -r '.[] | select(.permissions.pull == true) | .login')" 
-       
-#while read -r collab
-#do
- #   echo "$login"
-#done
-echo "$collab"
-}
-github_list
+#we use if else conditional statement to print user details or if no users 
+if [[ -z "$collab" ]]; then
 
+	echo "no users with read access: $owner/$repo"
+else
+	echo "users with read access to $owner/$repo"
+	echo "$collab"
+
+fi
